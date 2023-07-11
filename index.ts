8f72bf0f69a3bb8783c9dffbef54c79c55c7f324
santu1987 <gianni.d.santucci@gmail.com>
2023-07-11 17:11:43 -0400

    2 files modified, 1 added

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import './topics/4-character';
 import './topics/5-tarea-types';
 import './topics/6-destructuring';
+import './topics/7-destructuring-array';
 
 console.log('Hola mundo');
 
---
+++ topics/7-destructuring-array.ts
@@ -0,0 +1,4 @@
+//Destructyuring array: parecido al de objetos
+const [goku, vegeta, trunks = 'Not found'] = ['gokú', 'vegeta', 'trunks'];
+console.log('Personaje 3:', trunks);
+export {};
