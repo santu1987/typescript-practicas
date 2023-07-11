9f87531e95acfe48014d088ceb5c7408512e4781
santu1987 <gianni.d.santucci@gmail.com>
2023-07-11 18:17:56 -0400

    2 files modified, 1 added

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@ import './topics/4-character';
 import './topics/5-tarea-types';
 import './topics/6-destructuring';
 import './topics/7-destructuring-array';
-import './topics/8-destructuring-function';
+//import './topics/8-destructuring-function';
+import './topics/9-destructuring-functions-tarea';
 
 console.log('Hola mundo');
 
---
+++ topics/9-destructuring-functions-tarea.ts
@@ -0,0 +1,39 @@
+interface Product {
+  description: string;
+  price: number;
+}
+
+interface TaxCalculationsOptions {
+  tax: number;
+  products: Product[];
+}
+
+const phone: Product = {
+  description: 'Nokia A1',
+  price: 150.0,
+};
+
+const tablet: Product = {
+  description: 'ipad Air',
+  price: 250.0,
+};
+
+const shopingCart = [phone, tablet];
+const tax = 0.15;
+
+function taxtCalculation(options: TaxCalculationsOptions): [number, number] {
+  let total = 0;
+  const { tax, products } = options;
+  products.forEach(({ price }) => {
+    total += price;
+  });
+  return [total, tax];
+}
+
+const [total, taxTotal] = taxtCalculation({
+  products: shopingCart,
+  tax: tax,
+});
+
+console.log('Total', total);
+console.log('Taxt', taxTotal);
