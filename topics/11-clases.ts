41fe3dd6045d1d757f8f6451bf3774490e7a3ac0
santu1987 <gianni.d.santucci@gmail.com>
2023-07-14 16:29:56 -0400

    2 files modified

--- topics/11-clases.ts
+++ topics/11-clases.ts
@@ -1,13 +1,16 @@
 export class Person {
+  /*
   public name: string;
   private address: string;
-
-  constructor() {
-    this.name = 'Gianni Santucci';
-    this.address = 'Madrid';
-  }
+  //COmo se hace de forma redunante
+  constructor(name: string, address: string) {
+    this.name = name;
+    this.address = address;
+  }*/
+  //asignando los valores en la misma declaracion De esta misma forma se realiza la inyección de dependencias en angular
+  constructor(public name: string, private address: string = 'No Address') {}
 }
 
 //Declaramos a ironman como persona esa constante identifica como si Persona fuese una interfaz
-const ironman = new Person();
+const ironman = new Person('Ironman', 'new york');
 console.log(ironman);
