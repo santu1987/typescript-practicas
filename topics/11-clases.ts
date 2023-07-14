17e110b348e290bc4111f4349f899e894b01cf59
santu1987 <gianni.d.santucci@gmail.com>
2023-07-14 16:39:48 -0400

    2 files modified

--- topics/11-clases.ts
+++ topics/11-clases.ts
@@ -14,3 +14,19 @@ export class Person {
 //Declaramos a ironman como persona esa constante identifica como si Persona fuese una interfaz
 const ironman = new Person('Ironman', 'new york');
 console.log(ironman);
+
+//Extendiendo una clase
+
+export class Hero extends Person {
+  constructor(
+    public alterEgo: string,
+    public age: number,
+    public realName: string
+  ) {
+    //cons super se hace referencia a los atributos que se manejan en la clase a la cual se extiende nombre y direccion. SIn embargo se pueden crear nuevos campos como los señalados arriba y se crea el objeto con ellos
+    super(realName, 'New York');
+  }
+}
+
+const batman = new Hero('Batman', 45, 'Bruce Wayne');
+console.log(batman);
